c5dd4757b7ba77b93af74d0e834996d2c4abb9ac
Tox <yurikawsaoki@gmail.com>
2019-10-22 15:17:26 +0700

    Pick Balls

---
+++ PickBalls/PickBalls/Controller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board
+{
+    class Controller
+    {
+        private Player P1, P2;
+        private Board Game;
+
+        public Controller()
+        {
+            P1 = new Player("a");
+            P2 = new Player("b");
+            Game = new Board(3, 4, 6);
+        }
+
+        //play
+        public void PlayGame()
+        {
+            Game.PrintGame();
+            while (true)
+            {
+                Console.Write(P1.GetName() + "turn ");
+                P1.PickBalls(Game);
+                Game.PrintGame();
+                if (Game.IsGameOver())
+                {
+                    Console.WriteLine(P1.GetName() + "loses");
+                    Console.WriteLine(P2.GetName() + "Wins");
+                    break;
+                }
+                Console.Write(P1.GetName() + "turn ");
+                P2.PickBalls(Game);
+                Game.PrintGame();
+                if (Game.IsGameOver())
+                {
+                    Console.WriteLine(P2.GetName() + " Loses");
+                    Console.WriteLine(P1.GetName() + " Wins");
+                    break;
+                }
+            }
+        }
+
+    }
+}
---
+++ PickBalls/PickBalls/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board
+{
+    class Player
+    {
+        private string Name;//khai báo biến 
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+
+        //player pick balls
+        public void PickBalls(Board game)
+        {
+            Console.Write("which group do you choose? ");
+            int group = int.Parse(Console.ReadLine());
+            Console.Write("How many balls will you pick? ");
+            int balls = int.Parse(Console.ReadLine());
+            game.PickBall(group, balls);
+        }
+        public string GetName()
+        {
+            return this.Name;
+        }
+    }
+}
